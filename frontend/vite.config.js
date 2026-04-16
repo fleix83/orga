54947a50e49318b255d058a061c712ca0829fe65
Felix <fleix@gmx.ch>
2026-04-16 15:20:20 +0200

    feat: rebuild dist with all UI improvements

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/orga/dist/',
   build: {
     outDir: '../dist',
     emptyOutDir: true
@@ -10,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:80',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/orga/api')
       }
